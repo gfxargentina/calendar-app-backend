14333df5840873290989c0bc430a79d7a2b49dce
Luis Chilo <gfxargentina@gmail.com>
2021-05-15 11:14:13 -0300

    324 add public folder and config dotenv

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const express = require('express');
 
+// variables de entorno
+require('dotenv').config();
+
+
+//para ver todos los procesos que estan corriendo
+//console.log( process.env ) ;
+
 
 //Crear el servidor de express
 const app = express();
 
 
+//Directorio publico
+app.use( express.static('public') );
+
+
 //Rutas
-app.get('/', (req, res) => {
-    //console.log('Se requiere la ruta /');
-    res.json({
-        ok: true
-    })
-})
+// app.get('/', (req, res) => {
+//     //console.log('Se requiere la ruta /');
+//     res.json({
+//         ok: true
+//     })
+// })
 
 
 
 
 
 //Escuchar peticiones
-app.listen( 4000, () => {
-    console.log(`Servidor corriendo en puerto ${4000}`);
+app.listen( process.env.PORT , () => {
+    console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
 })
