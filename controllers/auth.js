d0f31556cd0635e249a2218186bb0bb8184783bb
Luis Chilo <gfxargentina@gmail.com>
2021-05-19 11:04:24 -0300

    327 post information

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,16 +2,27 @@ const { response } = require('express');
 
 
  const crearUsuario = (req, res = response ) => {
+
+   const { name, email, password } = req.body;
+
     res.json({ 
         ok: true,
-        msg: 'Nuevo Registro'
+        msg: 'Nuevo Registro',
+        name,
+        email,
+        password
     })
 }
 
 const loginUsuario = ( req, res = response ) => {
+
+    const { email, password } = req.body;
+
     res.json({
         ok: true,
-        msj: 'login'
+        msj: 'login',
+        email, 
+        password
     })
 }
 
--- index.js
+++ index.js
@@ -15,6 +15,9 @@ const app = express();
 //Directorio publico
 app.use( express.static('public') );
 
+//lectura y parseo del body
+app.use( express.json() );
+
 
 //Rutas (middlewares)
 app.use('/api/auth', require('./routes/auth'));
