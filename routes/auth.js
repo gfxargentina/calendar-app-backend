5a6a422698d7ac8313490d8466b4a7a8f6ebdeeb
Luis Chilo <gfxargentina@gmail.com>
2021-05-21 11:10:03 -0300

    329 add custom errros middleware

--- routes/auth.js
+++ routes/auth.js
@@ -13,21 +13,25 @@ const { check } = require('express-validator');
 const router = Router();
 
 
+
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 
 
 router.post('/new', 
-            [//middlewares
+            [//middlewares de validacion de express validator
                check('name', 'el nombre es obligatorio').not().isEmpty(),
                check('email', 'El email es obligatorio').isEmail(),
-               check('password', 'El password debe ser de mas de 5 caracteres').isLength({ min: 5 }) 
+               check('password', 'El password debe ser de mas de 5 caracteres').isLength({ min: 5 }),
+               validarCampos 
             ], 
             crearUsuario );
 router.post('/',
             [
                 check('email', 'el email es obligatorio').isEmail(),
-                check('password', 'El password debe ser de mas de 5 caracteres').isLength({ min:5 })
+                check('password', 'El password debe ser de mas de 5 caracteres').isLength({ min:5 }),
+                validarCampos
             ], 
             loginUsuario );
 router.get('/renew', revalidarToken );
