6fc8ba4e3e45d8044f9d6038183ad04e60be7d6c
Luis Chilo <gfxargentina@gmail.com>
2021-05-21 10:38:50 -0300

    328 express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,23 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 
  const crearUsuario = (req, res = response ) => {
 
    const { name, email, password } = req.body;
 
-    res.json({ 
+   //manejo de errores
+   const errors = validationResult( req );
+   //console.log(errors)
+   if ( !errors.isEmpty() ) {
+       return res.status(400).json({ 
+           ok: false,
+           errors: errors.mapped()
+       });
+   }
+   
+
+    res.status(201).json({ 
         ok: true,
         msg: 'Nuevo Registro',
         name,
@@ -18,7 +30,17 @@ const loginUsuario = ( req, res = response ) => {
 
     const { email, password } = req.body;
 
-    res.json({
+     //manejo de errores
+   const errors = validationResult( req );
+   
+   if ( !errors.isEmpty() ) {
+       return res.status(400).json({ 
+           ok: false,
+           errors: errors.mapped()
+       });
+   }
+
+    res.status(200).json({
         ok: true,
         msj: 'login',
         email, 
--- routes/auth.js
+++ routes/auth.js
@@ -9,14 +9,27 @@
 
 //desesestructuramos para hacer lo mismo que arriba que es lo mismo
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
+
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 
 
-router.post('/new', crearUsuario );
-router.post('/', loginUsuario );
+router.post('/new', 
+            [//middlewares
+               check('name', 'el nombre es obligatorio').not().isEmpty(),
+               check('email', 'El email es obligatorio').isEmail(),
+               check('password', 'El password debe ser de mas de 5 caracteres').isLength({ min: 5 }) 
+            ], 
+            crearUsuario );
+router.post('/',
+            [
+                check('email', 'el email es obligatorio').isEmail(),
+                check('password', 'El password debe ser de mas de 5 caracteres').isLength({ min:5 })
+            ], 
+            loginUsuario );
 router.get('/renew', revalidarToken );
 
 module.exports = router;
