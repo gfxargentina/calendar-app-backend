2cfe78e397e6b7216d66cd13db7d76cd6edaefd7
Luis Chilo <gfxargentina@gmail.com>
2021-05-22 10:19:26 -0300

    335 user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -7,8 +7,9 @@ const bcrypt = require('bcryptjs');
 
    const { email, password } = req.body;
 
-   try {
+     try {
           let usuario = await Usuario.findOne({email})  ;
+          //si el usuario existe en la db
           if ( usuario ) {
               return res.status(400).json({
                   ok:false,
@@ -41,16 +42,45 @@ const bcrypt = require('bcryptjs');
     
 }
 
-const loginUsuario = ( req, res = response ) => {
+const loginUsuario = async( req, res = response ) => {
 
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msj: 'login',
-        email, 
-        password
-    })
+    try {
+        const usuario = await Usuario.findOne({email});
+        //si el usuario no existe en la db
+          if ( !usuario ) {
+              return res.status(400).json({
+                  ok:false,
+                  msg: 'El usuario no existe con ese email'
+              })
+          }
+          //confirmar los passwords
+          const validPassword = bcrypt.compareSync( password, usuario.password );
+          //si el password no es igual
+          if ( !validPassword ) {
+              return res.status(400).json({
+                  ok:false,
+                  msg: 'Password incorrecto'
+              })
+          }
+
+          //generar nuestro JWT
+          res.json({
+              ok: true,
+              uid: usuario.id,
+              name: usuario.name
+          })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Hubo un Error, hable con el administrador'
+        })
+    }
+
+    
 }
 
 const revalidarToken = ( req, res = response ) => {
