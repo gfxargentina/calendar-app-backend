9901305f9012a0255c6ebfef47e81e6b77056e17
Luis Chilo <gfxargentina@gmail.com>
2021-05-24 20:41:23 -0300

    345 validate fields

--- controllers/events.js
+++ controllers/events.js
@@ -10,7 +10,7 @@ const getEventos= ( req, res = response ) => {
 const crearEvento= ( req, res = response ) => {
 
     //verificar que el evento llega
-    console.log( req.body)
+    //console.log( req.body)
 
     res.json({
         ok: true,
--- routes/events.js
+++ routes/events.js
@@ -4,8 +4,12 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { getEventos, crearEvento, actualizarEvento, borrarEvento } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
+const { validarCampos } = require('../middlewares/validar-campos');
 const { validarJWT } = require('../middlewares/validar-jwt');
+
 
 const router = Router();
 
@@ -17,7 +21,12 @@ router.use( validarJWT );
 router.get('/', getEventos );
 
 //crear un nuevo evento
-router.post('/', crearEvento );
+router.post('/',[
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+    check('end', 'Fecha de finalizacion es obligatoria').custom( isDate),
+    validarCampos
+], crearEvento );
 
 //actualizar evento 
 router.put('/:id', actualizarEvento );
