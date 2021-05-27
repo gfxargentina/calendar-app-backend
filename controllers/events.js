e168948737a0beb53af2e7fba11cc21b63c017ad
Luis Chilo <gfxargentina@gmail.com>
2021-05-27 10:23:17 -0300

    349 add delete events controller

--- controllers/events.js
+++ controllers/events.js
@@ -49,7 +49,7 @@ const actualizarEvento= async( req, res = response ) => {
 
         //verifica si el evento existe, si no existe tira un error
         if ( !evento ) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'No existe un evento con ese id'
             })
@@ -87,12 +87,48 @@ const actualizarEvento= async( req, res = response ) => {
     }
    }
 
-const borrarEvento= ( req, res = response ) => {
-    res.json({
-        ok: true,
-        msg: 'borrarEvento'
-    })
-}
+const borrarEvento= async( req, res = response ) => {
+    const eventoId = req.params.id;
+    const uid = req.uid;
+
+    try {
+        //busca el evento por id 
+        const evento = await Evento.findById( eventoId );
+
+        //verifica si el evento existe, si no existe tira un error
+        if ( !evento ) {
+           return res.status(404).json({
+                ok: false,
+                msg: 'No existe un evento con ese id'
+            })
+        }
+
+        //verificar si el usuario es el mismo que lo quiere modificar, si es asi lo graba en la db, si no es
+        //el mismo usuario tira un error y no lo graba en la db
+        if ( evento.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg:'No esta autorizado a eliminar este evento'
+            });
+        }
+
+        //borrar evento 
+        await Evento.findByIdAndDelete( eventoId );
+        res.json({
+            ok: true,
+            msg: 'Evento eliminado'
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+   }
+
 
 module.exports = {
     getEventos,
